94845f01b4da634ad94c816d2a6874d645987ba0
Elliot Berman <elliot@elliotjb.com>
2015-05-05 20:35:18 -0400

    Added Selection Sort

---
+++ SearchDemo/SortAlgorithms/SelectionSort.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchDemo.SortAlgorithms
+{
+    public class SelectionSort : Algorithm
+    {
+        private static string ExecuteBody = @"for (int i = 0; i < Integers.Count; i++)
+{
+    int smallestValueIndex = i;
+    for (int v = i; v < integers.Count; v++)
+    if (Integers[v] < Integers[smallestValueIndex]) smallestValueIndex = v;
+
+    int temp = Integers[i];
+    Integers[i] = Integers[smallestValueIndex];
+    Integers[smallestValueIndex] = temp;
+}";
+
+        public SelectionSort()
+            : base()
+        {
+            Name = "Selection Sort";
+            this.MethodBody = ExecuteBody;
+        }
+        public SelectionSort(ObservableCollection<int> integers) : base(integers) { }
+
+        public async override Task Execute()
+        {
+            for (int i = 0; i < Integers.Count; i++)
+            {
+                int smallestValueIndex = i;
+                for (int v = i; v < integers.Count; v++)
+                    if (Integers[v] < Integers[smallestValueIndex]) smallestValueIndex = v;
+
+                int temp = Integers[i];
+                Integers[i] = Integers[smallestValueIndex];
+                Integers[smallestValueIndex] = temp;
+
+                if (StepExecuted != null)
+                    await StepExecuted();
+            }
+        }
+    }
+}
---
+++ SearchDemo/ViewModel.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using SearchDemo.SortAlgorithms;
+
+namespace SearchDemo
+{
+    class ViewModel : INotifyPropertyChanged
+    {
+        public ViewModel()
+        {
+            Algorithms = new ObservableCollection<Algorithm>();
+            Algorithms.Add(new SelectionSort());
+            SelectedAlgorithm = Algorithms[0];
+            Randomize(null);
+        }
+
+
+        public ObservableCollection<Algorithm> Algorithms { get; private set; }
+
+        public ObservableCollection<int> Integers { get; private set; }
+
+        private string methodBody;
+        public string MethodBody
+        {
+            get { return methodBody; }
+            set
+            {
+                if(methodBody != value)
+                {
+                    methodBody = value;
+                    RaisePropertyChanged("MethodBody");
+                }
+            }
+        }
+
+        private int count = 10;
+        public int Count
+        {
+            get { return count; }
+            set
+            {
+                if(count != value)
+                {
+                    count = value;
+                    RaisePropertyChanged("Count");
+                    Randomize(null);
+                }
+            }
+        }
+
+        private Algorithm selectedAlgorithm;
+        public Algorithm SelectedAlgorithm
+        {
+            get { return selectedAlgorithm; }
+            set
+            {
+                if (selectedAlgorithm != value)
+                {
+                    selectedAlgorithm = value;
+                    MethodBody = selectedAlgorithm.MethodBody;
+                    RaisePropertyChanged("SelectedAlgorithm");
+                }
+            }
+        }
+
+        #region RandomizeCommand Members
+        public ICommand RandomizeCommand
+        {
+            get { return new Command(Randomize); }
+        }
+        private static Random rand = new Random();
+        private void Randomize(object obj)
+        {
+            if (Integers == null) Integers = new ObservableCollection<int>();
+            else Integers.Clear();
+
+            for (int i = 0; i < Count; i++) Integers.Add(rand.Next(Count) + 1);
+        }
+        #endregion
+
+        #region ExecuteCommand Members
+        public ICommand ExecuteCommand
+        {
+            get { return new Command(Execute, (object o) => { return selectedAlgorithm != null; }); }
+        }
+
+        private async void Execute(object obj)
+        {
+            selectedAlgorithm.StepExecuted = selectedAlgorithm_StepExecuted;
+            ObservableCollection<int> quick = new ObservableCollection<int>(Integers);
+            await selectedAlgorithm.Execute(Integers);
+            selectedAlgorithm.StepExecuted = null;
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            var task = selectedAlgorithm.Execute(quick);
+            stopwatch.Stop();
+        }
+
+        private async Task selectedAlgorithm_StepExecuted()
+        {
+            await Task.Delay(3000/Count);
+        }
+        #endregion
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged(string value)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+                handler(this, new PropertyChangedEventArgs(value));
+        }
+
+        #endregion
+    }
+}
