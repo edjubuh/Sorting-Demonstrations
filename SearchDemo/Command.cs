f3937920eb6d1cf3b6ae82dcd7901deb4cb7b692
Elliot Berman <elliot@elliotjb.com>
2015-05-05 20:33:51 -0400

    Added basic UI, Selection Sort

---
+++ SearchDemo/Command.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SearchDemo
+{
+    public class Command : ICommand
+    {
+        private Action<object> action;
+        private Func<object, bool> canExecute;
+
+        public Command(Action<object> action)
+        {
+            this.action = action;
+            this.canExecute = (object o) => { return true; };
+        }
+
+        public Command(Action<object> action, Func<object, bool> canExecute)
+        {
+            this.action = action;
+            this.canExecute = canExecute;
+        }
+
+        #region ICommand Members
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+            action(parameter);
+        }
+
+        #endregion
+    }
+}
---
+++ SearchDemo/SortAlgorithms/Algorithm.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchDemo.SortAlgorithms
+{
+    public delegate void ExecutedStepHandler(object sender, EventArgs e);
+
+    public abstract class Algorithm
+    {
+        protected ObservableCollection<int> integers;
+
+        public ObservableCollection<int> Integers
+        {
+            get
+            {
+                return integers;
+            }
+        }
+
+        public Func<Task> StepExecuted { get; set; }
+
+
+        public Algorithm() { }
+        public Algorithm(ObservableCollection<int> integers)
+        {
+            this.integers = integers;
+        }
+
+        public Task Execute(ObservableCollection<int> integers)
+        {
+            this.integers = integers;
+            return Execute();
+        }
+
+        public abstract Task Execute();
+
+        public string Name { get; protected set; }
+
+        public string MethodBody { get; protected set; }
+    }
+}
